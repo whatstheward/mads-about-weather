ea0d55a79abec94400c9c10ef1e004cd9e007e7e
github username <github email address>
2020-10-22 16:11:37 -0400

    get current weather for zip 30312

--- src/containers/WeatherCont.js
+++ src/containers/WeatherCont.js
@@ -1,22 +1,31 @@
 import { connect } from 'react-redux'
 import React from 'react'
 import {API_KEY} from '../adapters/api_adapter'
-import { addForecast } from '../actions/forecast'
+import { addCurrentWeather, addForecast } from '../actions/forecast'
 
 
 class WeatherCont extends React.Component {
 
-        getWeather =(zip)=>{
-        const query = `zip=${zip}&mode=json&units=imperial&APPID=${API_KEY}`
-        fetch(`http://api.openweathermap.org/data/2.5/forecast?${query}`)
-            .then(response => response.json())
-            .then(result => this.props.addForecast(result.list))
+        getForecast=(zip)=>{
+            const query = `zip=${zip}&mode=json&units=imperial&APPID=${API_KEY}`
+            fetch(`http://api.openweathermap.org/data/2.5/forecast?${query}`)
+                .then(response => response.json())
+                .then(result => this.props.addForecast(result.list))
+                .catch(error => console.log('error', error))
+        }
+
+        getWeather=(zip)=>{
+            const query = `zip=${zip},us&mode=json&units=imperial&APPID=${API_KEY}`
+            fetch(`http://api.openweathermap.org/data/2.5/weather?${query}`)
+            .then(res => res.json())
+            .then(data => this.props.addCurrentWeather(data))
             .catch(error => console.log('error', error))
         }
 
         componentDidMount(){
+            this.getForecast(30312)
             this.getWeather(30312)
-        }
+            }
 
         render(){
         return(
@@ -27,11 +36,13 @@ class WeatherCont extends React.Component {
 }
 
 const mapStateToProps = state => ({
-    forecast: state.forecasts
+    forecast: state.forecasts.forecasts,
+    currentWeather: state.forecasts.currentWeather
 })
 
 const mapDispatchToProps = dispatch =>({
-    addForecast: data => dispatch(addForecast(data))
+    addForecast: data => dispatch(addForecast(data)),
+    addCurrentWeather: data => dispatch(addCurrentWeather(data))
 })
 
 
